96e33f7f2d7e3b96bd29a8df89a0dcb5c1b87f7b
aersamkull <ehrsam@kull.ch>
2017-02-27 21:34:15 +0100

    Very basic server template

---
+++ server/src/main/java/ch/fhnw/cssr/CssrApplication.java
@@ -0,0 +1,12 @@
+package ch.fhnw.cssr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CssrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CssrApplication.class, args);
+	}
+}
---
+++ server/src/test/java/ch/fhnw/cssr/CssrApplicationTests.java
@@ -0,0 +1,16 @@
+package ch.fhnw.cssr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CssrApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
