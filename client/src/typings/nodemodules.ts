6039bd5d3946a0d483e5ac4e6d5185f915d6f2e3
aersamkull <ehrsam@kull.ch>
2017-03-04 17:54:15 +0100

    Swagger Codegen basically working

---
+++ client/src/typings/nodemodules.ts
@@ -0,0 +1,8 @@
+declare module "url" {
+    export function parse(url: string, t: boolean): any;
+    export function format(obj: any): string;
+}
+
+declare module "querystring" {
+    
+}
--- tools/swaggercodegen/generateClient.js
+++ tools/swaggercodegen/generateClient.js
@@ -1,38 +1,19 @@
 "use strict";
 const fs = require('fs');
+const fs_extra = require('fs-extra');
 const path = require('path');
 const http = require('http');
 const exec = require('child_process').exec;
 const spawn = require( 'child_process' ).spawn;
 
 // This is just a small utility that uses swager-codegen to generate typescript code
-// TODO: Use swagger-codegen-cli v 2.2.2 (fixes some relevant bugs)
+// the swagger-codegen-cli-custom.jar is generated from https://github.com/aersamkull/swagger-codegen
 // TODO: Use real swagger.json
 
-const download = function(url, dest, cb) {
-  var file = fs.createWriteStream(dest);
-  var request = http.get(url, function(response) {
-    response.pipe(file);
-    file.on('finish', function() {
-      file.close(cb);  // close() is async, call cb after close completes.
-    });
-  }).on('error', function(err) { // Handle errors
-    fs.unlink(dest); // Delete the file async. (But we don't check the result)
-    if (cb) cb(err.message);
-  });
-};
+generateCode(copyFile);
 
-if (!fs.existsSync("swagger-codegen-cli.jar")) {
-	download("http://central.maven.org/maven2/io/swagger/swagger-codegen-cli/2.2.1/swagger-codegen-cli-2.2.1.jar", "swagger-codegen-cli.jar", 
-	function() {
-		generateCode();
-	});
-}
-else {
-	generateCode();
-}
-function generateCode() {
-	var codegen = spawn( 'java', [ '-jar', 'swagger-codegen-cli.jar',
+function generateCode(cb) {
+	var codegen = spawn( 'java', [ '-jar', 'swagger-codegen-cli-custom.jar',
 		'generate',
 		'-i', 'swagger.json',
 		'-c', 'swaggerconfig.json',
@@ -49,6 +30,12 @@ function generateCode() {
 
 	codegen.on( 'close', code => {
 		console.log( `child process exited with code ${code}` );
-		process.exit();
+		if(cb) {
+			cb(); 
+		}
 	});
+}
+function copyFile() {
+	fs_extra.copySync('temp/api.ts', '../../client/src/services/api.ts');
+
 }
