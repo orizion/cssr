788a89d496ef07f2b9a0860fa678369eb4c994df
aersamkull <ehrsam@kull.ch>
2017-03-06 15:54:14 +0100

    project references on backend working

---
+++ server/domain/src/main/java/ch/fhnw/cssr/domain/UserRolesRepository.java
@@ -0,0 +1,15 @@
+package ch.fhnw.cssr.domain;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRolesRepository extends CrudRepository<UserRole, Long> {
+
+	@Query("select a.role from UserRole a inner join User b on b.userId=a.userId where b.email=?1")
+	public List<String> findRoleByEmail(String email);
+
+}
