27d869cc63dbc5e11f5266bb3f8dc5961f2aaabb
aersamkull <ehrsam@kull.ch>
2017-03-06 09:51:56 +0100

    simplier swagger

--- server/webserver/src/main/java/ch/fhnw/cssr/webserver/CssrApplication.java
+++ server/webserver/src/main/java/ch/fhnw/cssr/webserver/CssrApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.google.common.base.Predicates;
+
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -23,6 +26,7 @@ public class CssrApplication {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
+                .apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
                 .build();
     }
      
