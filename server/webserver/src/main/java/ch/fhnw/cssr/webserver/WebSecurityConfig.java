54cbe88e0b7dffe21f01f1cac1de8305f2db3dd3
aersamkull <ehrsam@kull.ch>
2017-03-06 12:17:17 +0100

    work on auth

--- server/webserver/src/main/java/ch/fhnw/cssr/webserver/WebSecurityConfig.java
+++ server/webserver/src/main/java/ch/fhnw/cssr/webserver/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package ch.fhnw.cssr.webserver;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,5 +14,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         // No authentication yet
     }
+	
+	/**
+	 * This section defines the user accounts which can be used for authentication as well as the roles each user has.
+	 * 
+	 * @see org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter#configure(org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder)
+	 */
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+	}
 	
 }
