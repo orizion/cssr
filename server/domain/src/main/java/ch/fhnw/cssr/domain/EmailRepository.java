e7a4fcb6494cc04b845cea576bdabe632a370edc
aersamkull <ehrsam@kull.ch>
2017-03-29 08:37:23 +0200

    Makes mailer executable

--- server/domain/src/main/java/ch/fhnw/cssr/domain/EmailRepository.java
+++ server/domain/src/main/java/ch/fhnw/cssr/domain/EmailRepository.java
@@ -1,10 +1,12 @@
 package ch.fhnw.cssr.domain;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmailRepository extends CrudRepository<Email, Long> {
 
-	
+	@Query("SELECT em FROM Email AS em WHERE em.sentDate IS NULL AND em.tryCount < 4")
+	public Iterable<Email> findNotSent();
 }
