44bd82baac65a99cc05be2aafb1677f70313d70d
aersamkull <ehrsam@kull.ch>
2017-04-03 13:17:28 +0200

    modification of script

--- tools/deploy/systemd.sh
+++ tools/deploy/systemd.sh
@@ -2,16 +2,16 @@
 
 # https://docs.spring.io/spring-boot/docs/current/reference/html/deployment-install.html
 
-$appdir = "/var/cssr/web"
-$user = "cssr" 
+appdir = "/var/cssr/web"
+appuser = "cssr" 
 
 if [ ! -d "$appdir" ]; then
    mkdir "$appdir"
-   chown $appdir $user
-   chgrp $appdir $user 
+   chown $appdir $appuser
+   chgrp $appdir $appuser 
    chmod 0400 $appdir # The user can only read
    
-   cp ./cssr/tools/deploy/cssr.service /etc/systemd/system 
+   sudo cp ./cssr/tools/deploy/cssr.service /etc/systemd/system 
 fi
 
-cp -f ./cssr/server/webserver.ressource/target/webserver.*.jar $appdir/webserver.ressource.jar
+sudo cp -f ./cssr/server/webserver.ressource/target/webserver.*.jar $appdir/webserver.ressource.jar
