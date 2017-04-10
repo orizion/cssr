919322569838f2ed4d6223888a0ab8e70e3d7c72
aersamkull <ehrsam@kull.ch>
2017-04-10 15:50:49 +0200

    now search works

--- server/domain/src/main/java/ch/fhnw/cssr/domain/UserRepository.java
+++ server/domain/src/main/java/ch/fhnw/cssr/domain/UserRepository.java
@@ -2,6 +2,7 @@ package ch.fhnw.cssr.domain;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -9,6 +10,6 @@ public interface UserRepository extends CrudRepository<User, Long> {
 	public User findByEmail(String email);
 
 	@Query("SELECT c FROM User AS c WHERE c.displayName LIKE CONCAT('%', :searchString, '%') OR c.email LIKE CONCAT('%', :searchString, '%')")
-    public Iterable<User> findByEmailOrDisplayName(String searchString);
+    public Iterable<User> findByEmailOrDisplayName(@Param("searchString") String searchString);
 
 }
