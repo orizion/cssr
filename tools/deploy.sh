b90fef2243312fb7630a7c7e312488d4e9aa146f
Stefan Mettler <stefan.mettler92@gmail.com>
2017-05-15 14:48:13 +0200

    Added Frontend to deployment script

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,11 +1,14 @@
 set -e
 
+cd ../client
+cp -r dist /var/www/html
+cp index.html /var/www/html
 cd ../server/domain
 mvn install
 cd ../mailer
 mvn package
 cd ../webserver.ressource/
 mvn package
-
 cd ../../
+
 sudo ./cssr/tools/deploy/systemd.sh
