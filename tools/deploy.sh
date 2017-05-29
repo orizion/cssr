01045a8853652857a90278b1764ff0427c501ace
aersamkull <ehrsam@kull.ch>
2017-05-29 10:01:25 +0200

    better mails

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -9,6 +9,7 @@ sudo cp -r node_modules/react* /var/www/html/node_modules
 sudo cp -r style /var/www/html
 sudo cp -r dist /var/www/html
 sudo cp index.html /var/www/html
+sudo cp *.png /var/www/html
 cd ../server/domain
 mvn install
 cd ../mailer
