82e85f3a752f021f32d3003047443acb05fca328
aersamkull <ehrsam@kull.ch>
2017-05-29 12:55:07 +0200

    deploy imp

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -5,11 +5,11 @@ set -e
 cd ../client
 yarn install
 npm run build
-sudo cp -r node_modules/react* /var/www/html/node_modules
-sudo cp -r style /var/www/html
-sudo cp -r dist /var/www/html
-sudo cp index.html /var/www/html
-sudo cp *.png /var/www/html
+sudo cp -r node_modules/react* /var/www/html/wodss17-6/node_modules
+sudo cp -r style /var/www/html/wodss17-6
+sudo cp -r dist /var/www/html/wodss17-6
+sudo cp index.html /var/www/html/wodss17-6
+sudo cp *.png /var/www/html/wodss17-6
 cd ../server/domain
 mvn install
 cd ../mailer
