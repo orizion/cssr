2c241fc45fb38ef8ffe4c7cd6a2e5c7f432e89fb
aersamkull <ehrsam@kull.ch>
2017-05-29 08:12:41 +0200

    style to separate file

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -2,6 +2,7 @@ set -e
 
 cd ../client
 sudo cp -r node_modules /var/www/html
+sudo cp -r style /var/www/html
 sudo cp -r dist /var/www/html
 sudo cp index.html /var/www/html
 cd ../server/domain
@@ -10,6 +11,6 @@ cd ../mailer
 mvn package
 cd ../webserver.ressource/
 mvn package
-cd ../../
+cd ../../../
 
 sudo ./cssr/tools/deploy/systemd.sh
